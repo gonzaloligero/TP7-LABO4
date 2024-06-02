169ec8aacc58ecb44abccc1a49977d8fb7079ce2
draganoev <gonzaloligero@gmail.com>
2024-06-02 18:08:34 -0300

    Update arreglando errores

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,7 @@
-import dao.ContratacionDao;
-import dao.SeguroDao;
-import dao.TipoSeguroDao;
-import dominio.Contratacion;
+package src;
+
+import src.dao.*;
+import src.dominio.*;
 
 
 public class Principal {
@@ -10,15 +10,15 @@ public class Principal {
 		// TODO Auto-generated method stub
 		//SeguroDao sDao = new SeguroDao();
 		//TipoSeguroDao tsDao = new TipoSeguroDao();
-		ContratacionDao tsDao2 = new ContratacionDao();
-		Contratacion c = new Contratacion();
-		c.setNombreUsuario("Gaston");
-		c.setIdSeguro(1);
-		c.setCostoContratacion(2950);
-		tsDao2.agregarContratacion(c);
+		//ContratacionDao tsDao2 = new ContratacionDao();
+		//Contratacion c = new Contratacion();
+		//c.setNombreUsuario("Gaston");
+		//c.setIdSeguro(1);
+		//c.setCostoContratacion(2950);
+		//tsDao2.agregarContratacion(c);
 		//System.out.println(sDao.obtenerUsuarios());
 		//System.out.println(tsDao.listarSeguros());
-		System.out.println(tsDao2.listarContratacion());
+		//System.out.println(tsDao2.listarContratacion());
 		
 		//int f = sDao.agregarSeguro(new Seguro("Segupor por recursar", 3, 900, 1700));
 	}
